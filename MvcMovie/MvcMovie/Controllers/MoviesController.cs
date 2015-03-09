345cd0b323d8455e4c8dfc853790d14172d5b8e1
Julian Egbert <jjegbe01@louisville.edu>
2015-03-09 19:31:18 -0400

    Completed first step of part 8 of the tutorial

--- MvcMovie/MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/MvcMovie/Controllers/MoviesController.cs
@@ -15,8 +15,15 @@ namespace MvcMovie.Controllers
         private MovieDBContext db = new MovieDBContext();
 
         // GET: /Movies/
-        public ActionResult Index()
+        public ActionResult Index(string searchString)
         {
+            var movies = from m in db.Movies
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
             return View(db.Movies.ToList());
         }
 
